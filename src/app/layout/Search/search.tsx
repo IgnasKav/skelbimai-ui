5c3c672f7bb7e21241d21e96b7649a88bfa70ff9
KarkiusX <kulkasta7@gmail.com>
2021-05-22 15:56:21 +0300

    Fix

--- src/app/layout/Search/search.tsx
+++ src/app/layout/Search/search.tsx
@@ -1,36 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import css from './search.module.scss';
 import Button from '@material-ui/core/Button';
 import {useHistory} from "react-router-dom";
 import {useStore} from 'app/stores/store';
-import categoryStore from "../../stores/categoryStore";
+import LoadingComponent from '../loadingComponent';
 
 export default function Header() {
-    const {advertisementStore} = useStore();
+    const {advertisementStore, categoryStore} = useStore();
+
+    const {categories} = categoryStore;
 
     const [searchField, setSearch] = useState<string>();
+    const [filterField, setFilter] = useState<string>();
 
     const GetSearch = (event: any) => {
         setSearch(event.target.value);
     }
+
     const Find = () =>{
         advertisementStore.searching = true;
         advertisementStore.loadAdvertisements(searchField)
     }
-    const [filterField, setFilter] = useState<string>();
 
     const GetFilter = (event: any) => {
         if(event.target.value === "None")
             setFilter("");
         setFilter(event.target.value)
     }
-    const FilterByCategory = () => {
-        advertisementStore.searching = true;
+    const FilterByCategory = (event: any) => {
         advertisementStore.loadAdvertisements(filterField)
     }
 
     return (
         <>
+        {
+            console.log()
+        }
             <div className={css.search}>
                 <div className={css.field}>
                     <input type="text" className={css.textField} placeholder={"Enter name"} onChange={GetSearch}/>
@@ -40,9 +45,15 @@ export default function Header() {
                     <option value="volvo">Ascending</option>
                     <option value="saab">Descending</option>
                     </select>
-                    <span style={{fontSize: "16px"}} onChange={() =>GetFilter }> FilterCategory</span>
-                    <select className={css.textField} id="filter" name="filter">
+                    <span style={{fontSize: "16px"}}> FilterCategory</span>
+                    <select className={css.textField} id="filter" name="filter" onChange={FilterByCategory}>
                     <option value="-1">None</option>
+                    {
+                        categories.map(category =>
+                            (
+                                <option value={category.id}>{category.name}</option>
+                            ))
+                    }
                     </select>
                 </div>
                 <div className={css.button}>
