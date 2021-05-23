c3f2f8b52afc160c9c97b2a6a3626d99183d6f93
Ignas Kavaliauskas <ignas.kavaliauskas5@gmail.com>
2021-05-24 01:19:18 +0300

    make login prettier, add validation

--- src/app/features/users/login-form.tsx
+++ src/app/features/users/login-form.tsx
@@ -1,33 +1,51 @@
-import MyTextInput from 'app/common/form/MyTextInput'
 import {useStore} from 'app/stores/store'
-import {ErrorMessage, Formik} from 'formik'
+import {useFormik} from 'formik'
 import css from './login-form.module.scss'
 import {observer} from 'mobx-react-lite'
-import React from 'react'
-import {Button, Form, Label} from 'semantic-ui-react'
-
+import React, {useState} from 'react'
+import CommonInput from "app/shared/inputs/common-input/common-input-field";
+import * as Yup from 'yup';
+import {UserFormValues} from "app/models/user";
+import {Button, Card} from "@material-ui/core";
+import FormError from "app/shared/inputs/form-error/form-error";
+import {history} from "index";
 
 export default observer(function LoginForm() {
     const {userStore} = useStore();
+    const [formError, setFormError] = useState<string>("");
+
+    const submit = (values: UserFormValues) => {
+        userStore.login(values).catch(error => setFormError( 'Invalid email or password'))
+    }
+
+    const formik = useFormik({
+        initialValues: {email: '', password: '', error: null},
+        validationSchema: Yup.object({
+            email: Yup.string().email('Neteisingas el.pašto formatas').required('*'),
+            password: Yup.string().required('*')
+        }),
+        onSubmit: (values) => submit(values)
+    });
+
+
     return (
-        <div className={css.loginForm}>
-            <Formik
-                initialValues={{email: '', password: '', error: null}}
-
-                onSubmit={(values, {setErrors}) => userStore.login(values).catch(error => setErrors({error: 'Invalid email or password'}))}>
-                {({handleSubmit, isSubmitting, errors}) => (
-                    <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
-                        <MyTextInput name='email' placeholder='Email'/>
-                        <MyTextInput name='password' placeholder='Pass' type='password'/>
-                        <ErrorMessage name='error' render={() =>
-                            <Label style={{marginBottom: 10}} basic color='red' content={errors.error}/>}
-                        />
-                        <Button loading={isSubmitting} positive content='Login' type='submit' fluid/>
-                    </Form>
-
-                )}
-            </Formik>
-        </div>
-
+        <Card className={css.loginForm}>
+            <h1>Prisijungimas</h1>
+            <form onSubmit={formik.handleSubmit} autoComplete='off'>
+                <CommonInput label="E-mail" name="email" value={formik.values.email} onChange={formik.handleChange}
+                             error={formik.errors.email}/>
+                <CommonInput label="Slaptažodis" name="password" type="password" value={formik.values.password}
+                             onChange={formik.handleChange} error={formik.errors.password}/>
+                <FormError error={formError}/>
+                <div className={css.formButtons}>
+                    <Button variant="outlined" color="secondary" onClick={() => history.push('/register')}>
+                        Registracija
+                    </Button>
+                    <Button variant="outlined" color="primary" type="submit">
+                        Prisijungti
+                    </Button>
+                </div>
+            </form>
+        </Card>
     )
 })
--- src/app/features/users/register/register-form.tsx
+++ src/app/features/users/register/register-form.tsx
@@ -1,36 +1,60 @@
-import MyTextInput from 'app/common/form/MyTextInput'
 import {useStore} from 'app/stores/store'
-import {ErrorMessage, Formik} from 'formik'
+import {useFormik} from 'formik'
 import css from '../login-form.module.scss'
 import {observer} from 'mobx-react-lite'
-import React from 'react'
-import {Button, Form, Label} from 'semantic-ui-react'
+import React, {useState} from 'react'
+import * as Yup from "yup";
+import {UserFormValues} from "app/models/user";
+import CommonInput from "app/shared/inputs/common-input/common-input-field";
+import FormError from "app/shared/inputs/form-error/form-error";
+import {Card, Button} from "@material-ui/core";
+import {history} from "../../../../index";
 
 export default observer(function RegisterForm() {
     const {userStore} = useStore();
+
+    const [formError, setFormError] = useState<string>("");
+
+    const submit = (values: UserFormValues) => {
+        userStore.register(values).catch(error => setFormError(error))
+    }
+
+    const formik = useFormik({
+        initialValues: {displayName: '', username: '', email: '', password: '', error: null},
+        validationSchema: Yup.object({
+            displayName: Yup.string().required('*'),
+            username: Yup.string().required('*'),
+            email: Yup.string().email('Neteisingas el.pašto formatas').required('*'),
+            password: Yup.string().required('*')
+        }),
+        onSubmit: (values) => submit(values)
+    });
+
     return (
-        <div className={css.loginForm}>
-            <Formik
-                initialValues={{displayName: '', username: '', email: '', password: '', error: null}}
-
-                onSubmit={(values, {setErrors}) => userStore.register(values).catch(error => {
-                    setErrors({error: 'Invalid email or password'});
-                })}>
-                {({handleSubmit, isSubmitting, errors}) => (
-                    <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
-                        <MyTextInput name='displayName' placeholder='Display Name'/>
-                        <MyTextInput name='username' placeholder='Username'/>
-                        <MyTextInput name='email' placeholder='Email'/>
-                        <MyTextInput name='password' placeholder='Pass' type='password'/>
-                        <ErrorMessage name='error' render={() =>
-                            <Label style={{marginBottom: 10}} basic color='red' content={errors.error}/>}
-                        />
-                        <Button loading={isSubmitting} positive content='Registruotis' type='submit' fluid/>
-                    </Form>
-
-                )}
-            </Formik>
-        </div>
+        <Card className={css.loginForm}>
+            <h1>Registracija</h1>
+            <form onSubmit={formik.handleSubmit} autoComplete='off'>
+                <CommonInput label="Display Name" name="displayName" value={formik.values.displayName}
+                             onChange={formik.handleChange}
+                             error={formik.errors.displayName}/>
+                <CommonInput label="Username" name="username" value={formik.values.username}
+                             onChange={formik.handleChange}
+                             error={formik.errors.username}/>
+                <CommonInput label="E-mail" name="email" value={formik.values.email} onChange={formik.handleChange}
+                             error={formik.errors.email}/>
+                <CommonInput label="Slaptažodis" name="password" type="password" value={formik.values.password}
+                             onChange={formik.handleChange} error={formik.errors.password}/>
+                <FormError error={formError}/>
+                <div className={css.formButtons}>
+                    <Button variant="outlined" color="secondary" onClick={() => history.push('/login')}>
+                        Prisijungimas
+                    </Button>
+                    <Button variant="outlined" color="primary" type="submit">
+                        Registruotis
+                    </Button>
+                </div>
+            </form>
+        </Card>
 
     )
 })
